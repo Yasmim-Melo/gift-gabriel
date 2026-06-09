6fafb0d01a8c4e075188c0d8309ee0bc35f9fca7
Yasmim Andrade de Melo <108697501+Yasmim-Melo@users.noreply.github.com>
2026-06-09 15:23:51 -0300

    chore: configurar Supabase do album online

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://xzptgptsrtouheqmmfjb.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_SbD7UYGq5GTFvn--xcKJRw_F9F5kXfc",
   SUPABASE_BUCKET: "love-photos"
 };
